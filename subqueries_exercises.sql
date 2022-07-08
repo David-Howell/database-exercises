845e88a41316efc3f0c8c5ffd96734d20d9bfa88
David Howell <davidallenhowell@gmail.com>
2022-07-07 21:45:14 -0500

    Updates, cleaning, NOT DONE... but here we are

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -11,11 +11,11 @@
             FROM dept_emp de
             JOIN employees e
             ON e.emp_no = de.emp_no
-            AND hire_date = (
-				SELECT hire_date
-				FROM employees
-				WHERE emp_no = 101010)
-			AND de.to_date > NOW()
+				AND hire_date = (
+					SELECT hire_date
+					FROM employees
+					WHERE emp_no = 101010)
+				AND de.to_date > NOW()
             ;
 
 /*			2.	Find all the titles ever held by all current employees with the first name Aamod.*/
@@ -57,10 +57,13 @@
 
 /*			5.	Find all the employees who currently have a higher salary than the companies overall, historical average salary.*/
 
-/*			6.	How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
-
+/*			6.	How many current salaries are within 1 standard deviation of the current highest salary? 
+					(Hint: you can use a built in function to calculate the standard deviation.) 
+                    •	What percentage of all salaries is this?
 					•	Hint You will likely use multiple subqueries in a variety of ways
-					•	Hint It's a good practice to write out all of the small queries that you can. Add a comment above the query showing the number of rows returned. You will use this number (or the query that produced it) in other, larger queries.*/
+					•	Hint It's a good practice to write out all of the small queries that you can. 
+						Add a comment above the query showing the number of rows returned. 
+                        You will use this number (or the query that produced it) in other, larger queries.*/
 /*			BONUS
 
 /*			1.	Find all the department names that currently have female managers.*/
